4ed6c22b546414ceb34d4c69d379fcf03e5087a0
Peter <pjthompson.au@gmail.com>
2020-03-02 17:24:53 +1100

    Car Physics

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(InputManager))]
+public class CarController : MonoBehaviour
+{
+    public InputManager im;
+    public List<WheelCollider> throttleWheels;
+    public List<GameObject> steeringWheels;
+    public List<GameObject> meshes;
+
+    public float strengthCoefficient = 20000f;
+    public float maxTurn = 20f;
+
+    public Transform CM;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        im = GetComponent<InputManager>();
+        rb = GetComponent<Rigidbody>();
+
+        if (CM)
+        {
+            rb.centerOfMass = CM.localPosition;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        foreach (WheelCollider wheel in throttleWheels)
+        {
+            wheel.motorTorque = strengthCoefficient * Time.deltaTime * im.throttle;
+        }
+
+        foreach (GameObject wheel in steeringWheels)
+        {
+            wheel.GetComponent<WheelCollider>().steerAngle = maxTurn * im.steer;
+            wheel.transform.localEulerAngles = new Vector3(0f, im.steer * maxTurn, 0f);
+        }
+    }
+}
